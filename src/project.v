9529c64f3dbe16f9d646bafb9f3d0153cdfc4825
Vivi Huang <144827960+Ctr-cv@users.noreply.github.com>
2025-06-24 17:02:57 -0400

    Update project.v

--- src/project.v
+++ src/project.v
@@ -16,8 +16,8 @@ module tt_um_BNN (
     input  wire       rst_n     // Active-low reset
 )
 // --------------- Constants set for BNN ------------------------
-localparam NUM_NEURONS = 4;
-localparam NUM_WEIGHTS = 6;
+parameter NUM_NEURONS = 4;
+parameter NUM_WEIGHTS = 6;
 
 wire reset = ~rst_n; // use active-high reset
 wire [5:0] data = {2'b0, ui_in[7:2]}; // 00 + 6-bit input
